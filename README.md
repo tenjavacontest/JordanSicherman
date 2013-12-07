dbd70e518d2c85414185e32bb0538f3a8a5e3e3c
TeeePeee <jordan.sicherman@brentwood.bc.ca>
2013-12-07 07:25:36 -0800

    Last commit with some clean-up and a name check.
    I wish I could have participated more, unfortunately, time is not on my
    side.

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-Theme:
+Theme: Books
 
 Time: 8:00 UTC
 
 Commands and Permissions: Will be clearly documented in plugin.yml and easily viewable via. an in-game command.
 
 OH NO! I slept in! If only this was a 1-hour competition that started now.
+
+Purpose: To allow people to read Shakespeare via. books in Minecraft. Unfortunately, due to restraints with Minecraft, the format is horrible.
+
+Credits: All books are written by William Shakespeare and provided online at http://shakespeare.classicauthors.net by Great Literature Online.
--- src/ten/java/JordanSicherman/Commands/BookCommand.java
+++ src/ten/java/JordanSicherman/Commands/BookCommand.java
@@ -19,15 +19,26 @@ public class BookCommand implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (sender instanceof Player) {
+			// Send a list of books if we don't provide one in the arguments.
 			if (args.length == 0) {
 				for (String book : Utilities.getListOfBooks().keySet())
 					Utilities.sendMessage(sender, "&e" + book);
 				return true;
 			}
+
+			// Find the name.
 			String name = "";
 			for (String arg : args)
 				name += arg + " ";
 			name = name.trim();
+
+			// Make sure the book exists.
+			if (!Utilities.getListOfBooks().containsKey(name)) {
+				Utilities.sendMessage(sender, "&4That book doesn't exist!");
+				return true;
+			}
+
+			// Construct the book.
 			Utilities.constructBook((Player) sender, name);
 		}
 		return true;
